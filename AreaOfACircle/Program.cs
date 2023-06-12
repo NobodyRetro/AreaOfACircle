dd0df1c5da1a1cc97dee6860def88f2e0753fde1
NobodyRetro <59867447+NobodyRetro@users.noreply.github.com>
2023-06-12 02:33:04 -0700

    Add files via upload

---
+++ AreaOfACircle/Program.cs
@@ -0,0 +1,7 @@
+﻿const double PI = 3.14;
+Console.WriteLine("Give me the radius and I will give you the area of the cirlce.");
+double radius = Convert.ToDouble(Console.ReadLine());
+var AreaOfCirlce = radius * radius * PI;
+Console.WriteLine("The area of the circle is: " + AreaOfCirlce);
+Console.WriteLine("Press any key to exit.");
+Console.ReadKey();
